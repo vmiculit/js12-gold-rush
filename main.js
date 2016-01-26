30c6afc8224053242410480744018361751221e6
Evan Brauch <evan.brauch@gmail.com>
2016-01-26 12:24:56 -0700

    part 1 complete

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ angular.module("js12",[])
 angular.module("js12")
 	.controller("js12Troller",["$scope", "$filter", "$window", "$timeout", function($scope, $filter, $window, $timeout) {
 
-		$scope.mouseClickValues = [
-			{x : 0},
-			{y : 0}
-		]
-
+		$scope.goldFinds = []
 
 
 		$scope.mouseClick = function(event) {
-			mouseClickValues[0].x = event.clientX
-						mouseClickValues[0].x = event.clientX
-
-			console.log(mouseClickValues)
+			var x = event.clientX - 5
+			var y = event.clientY - 5
+			var id = x * y
+			$scope.goldFinds.push({id: id,
+							x: x,
+							y: y})
+			console.log($scope.goldFinds)
 		}
 
 	}])
