09836294bb15b0a41225dfd95723c12acfd1f1cf
Valentin Miculit <valsbox@gmail.com>
2016-01-26 16:32:00 -0700

    bonus 1

--- main.js
+++ main.js
@@ -10,12 +10,41 @@ angular.module("js12")
 			var positionOffset = 14
 			var x = event.pageX - positionOffset
 			var y = event.pageY - positionOffset
+			var notes = ""
 
-			$scope.goldFinds.push({ x: x, y: y})
+			$scope.goldFinds.push({ x: x, y: y, notes: notes, shownote: false, showinput: true})
+			console.log($scope.goldFinds)
 		}
 
 		$scope.removeMarker = function(idx) {
+			if ($scope.goldFinds[idx].notes != "") {
 			$scope.goldFinds.splice(idx, 1)
+			}
+		}
+
+		$scope.displayNote = function(idx) {
+
+			if ($scope.goldFinds[idx].notes != "") {
+
+			$scope.goldFinds[idx].shownote = true
+
+			}
+		}
+
+		$scope.hideNote = function(idx) {
+
+			if ($scope.goldFinds[idx].notes != "") {
+
+			$scope.goldFinds[idx].shownote = false
+
+			}
+		}
+
+		$scope.addNote = function(idx, placeNote) {
+
+			$scope.goldFinds[idx].notes = placeNote
+			$scope.goldFinds[idx].showinput = false
+
 		}
 
 	}])
